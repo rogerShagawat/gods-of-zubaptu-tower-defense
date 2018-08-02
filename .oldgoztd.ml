5d925533e5b146b5292fffa0ada640e37bcb480c
rogerShagawat <34222430+rogerShagawat@users.noreply.github.com>
2018-08-02 15:23:20 -0400

    GRPAHICS U MOTHER FATHER

--- .oldgoztd.ml
+++ .oldgoztd.ml
@@ -12,7 +12,7 @@ type tile =
 | Path of coordinate
 | Floor of coordinate
 | Enemy of int*int
-| TowerFirst of coordinate
+| Tower of coordinate
 | Nothing of coordinate
 
 type level = 
@@ -27,9 +27,21 @@ type level =
 
 type direction = North | South | East | West
 
-(*-----------------------*)
-(*        Types          *)
+(*           Levels            *)
+(*-----------------------------*)
 
+let level_1 ={
+    path =        [Path(0,0);Path(0,1);Path(0,2);Path(0,3);Path(0,4);Path(0,5);Path(0,6);Path(0,7);Path(0,8);Path(0,9)];
+    path_start =  (0,0);
+    path_end =    (0,0);
+    lives =       1000;
+    enemy_list =  [];
+    tower_first = [];
+    wave = [1;2;3;4;5;10;200;500;800;700]
+    }
+    
+(*        Functions         *)
+(*--------------------------*)
 
 let next_pos (x,y) d = 
 match d with
@@ -37,17 +49,6 @@ match d with
 | South -> (x,y-1)
 | East -> (x+1,y)
 | West -> (x-1,y)
-
-let is_free_path (c:tile) (lev:level) = List.mem c lev.path
-
-let tile_to_string t =
-match t with
-| Enemy(h,_) -> string_of_int h
-| Path(_,_) -> "="
-| Floor(_,_) -> "x"
-| TowerFirst(_,_) -> "T"
-| Nothing(_,_) -> "\n"
-| _ -> failwith "expected tile"
 
 let rec lives_lost enemy_lst path =
   match enemy_lst with
@@ -74,7 +75,7 @@ let enemy_list_pos_to_coord enemy_lst path =
 
 let distance_t x1 y1 t =
   match t with
-  |TowerFirst (x2,y2)-> 
+  |Tower (x2,y2)-> 
     truncate (floor(sqrt (((float_of_int(x1)-.float_of_int(x2))**2.0)+.((float_of_int(y1)-.float_of_int(y2))**2.0))))
 
 let rec enemies_in_range tower enemy_c_l path range= 
@@ -84,17 +85,6 @@ let rec enemies_in_range tower enemy_c_l path range=
     if distance_t x y tower <= range
     then (Path(x,y))::enemies_in_range tower tail path range
     else enemies_in_range tower tail path range
-
-(*entire grid 12X12*)
-let level_1 ={
-    path =        [Path(0,0);Path(0,1);Path(0,2);Path(0,3);Path(0,4);Path(0,5);Path(0,6);Path(0,7);Path(0,8);Path(0,9)];
-    path_start =  (0,0);
-    path_end =    (0,0);
-    lives =       100;
-    enemy_list =  [];
-    tower_first = [];
-    wave = [1;2;3;4;5;6]
-    }
 
 let move_enemies enemy_list =
   List.map (fun (Enemy(h,l)) -> (Enemy(h,(l+1)))) enemy_list
@@ -106,7 +96,7 @@ let spawn_enemy (w:int list) =
 
 let remove_first l =
 match l with
-| []->[]
+| [] -> []
 | head::tail -> tail
 
 let rec remove_dead_enemy (l:tile list) =
@@ -120,6 +110,15 @@ match l with
 (*          Graphics          *)
 (*----------------------------*)
 
+let tile_to_string t =
+match t with
+| Enemy(h,p) -> string_of_int h
+| Path(_,_) -> "="
+| Floor(_,_) -> "x"
+| Tower(_,_) -> "T"
+| Nothing(_,_) -> "\n"
+| _ -> failwith "expected tile"
+
 let rec list_to_string l =
 match l with
 | [] -> []
@@ -130,8 +129,35 @@ match n with
 | 0 -> []
 | _ -> (Path(0,0))::construct_path_helper (n-1)
 
+let rec list_pos_enemy (el:tile list) =
+match el with
+| [] -> []
+| Enemy(_,p)::tail -> p::(list_pos_enemy tail)
+
+let rec find_lowest_pos (el_pos:int list) (current_max:int) =
+match el_pos with
+| [] -> current_max
+| head::tail -> find_lowest_pos tail (if head < current_max then head else current_max)
+
+let rec find_highest_pos (el_pos:int list) (current_max:int) =
+match el_pos with
+| [] -> current_max
+| head::tail -> find_highest_pos tail (if head > current_max then head else current_max)
+
 let construct_path (el:tile list) (pl:tile list) = 
-el @ (construct_path_helper ((List.length pl) - (List.length el)))
+  let lpos = 
+  (if List.length el > 0
+  then find_lowest_pos (list_pos_enemy el) (100)
+  else 0)
+  in let hpos = find_highest_pos (list_pos_enemy el) (-1)
+  
+  in let before_enemies =(List.length pl - (List.length pl - lpos))
+  in let after_enemies =(List.length pl - (hpos-1))
+
+  in ((construct_path_helper before_enemies) @ (List.rev el) @ (construct_path_helper after_enemies))
+
+  (*(List.length el - hpos -1)) @ (el) @ (construct_path_helper ((List.length pl) - (hpos+1)))*)
+  
 
 let construct_level (lev:level) =
 print_string ("Towers:" ^(string_of_int (List.length (lev.tower_first)))^"\nLives: "
@@ -160,9 +186,9 @@ let winning_state (lev:level) =
 let rec main_loop (lev:level) = 
   Unix.sleepf(0.5);
   if loosing_state lev
-    then print_string ("YOU LOOSE"^(string_of_int lev.lives))
+    then print_string ("YOU LOSE!!!")
   else if winning_state lev
-    then print_string ("YOU WIN!!!" ^(string_of_int lev.lives))
+    then print_string ("YOU WIN!!!")
   else
     let lev = apply_update_level lev
     in construct_level lev;
