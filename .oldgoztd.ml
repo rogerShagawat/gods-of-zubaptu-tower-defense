41d1f26efcb4c2b996c58253de1e5f0b1db40801
rogerShagawat <34222430+rogerShagawat@users.noreply.github.com>
2018-08-02 09:56:57 -0400

    Add files via upload

--- .oldgoztd.ml
+++ .oldgoztd.ml
@@ -21,7 +21,9 @@ type level =
     path_end:     coordinate;
     lives:        int;
     enemy_list:   tile list;
-    tower_first:  tile list;}
+    tower_first:  tile list;
+    wave:         int list;
+    }
 
 type direction = North | South | East | West
 
@@ -40,7 +42,7 @@ let is_free_path (c:tile) (lev:level) = List.mem c lev.path
 
 let tile_to_string t =
 match t with
-| Enemy(_,_) -> "1"
+| Enemy(h,_) -> string_of_int h
 | Path(_,_) -> "="
 | Floor(_,_) -> "x"
 | TowerFirst(_,_) -> "T"
@@ -52,34 +54,55 @@ match l with
 | [] -> []
 | head::tail -> (tile_to_string head)::(list_to_string_helper tail) 
 
-let rec level_1_path_init (l:tile list) =
-match l with
-| [] -> []
-| Path(x,y)::tail-> Path(x,y)::level_1_path_init tail
+let rec lives_lost enemy_lst path =
+  match enemy_lst with
+  | [] -> 0
+  | Enemy(h,l)::tail ->
+    if List.length path < l
+    then h + lives_lost tail path
+    else lives_lost tail path
 
-let level_1_board_init = [
-  Floor(0,0);Floor(0,1);Floor(0,2);Floor(0,3);Floor(0,4);Floor(0,5);Path(0,6);Floor(0,7);
-  Floor(1,0);Floor(1,1);Floor(1,2);Floor(1,3);Floor(1,4);Floor(1,5);Path(1,6);Floor(1,7);
-  Floor(2,0);Path(2,1);Path(2,2);Path(2,3);Floor(2,4);Floor(2,5);Path(2,6);Floor(2,7);
-  Floor(3,0);Path(3,1);Floor(3,2);Path(3,3);Floor(3,4);Floor(3,5);Path(3,6);Floor(3,7);
-  Floor(4,0);Path(4,1);Floor(4,2);Path(4,3);Path(4,4);Path(4,5);Path(4,6);Floor(4,7);
-  Floor(5,0);Path(5,1);Floor(5,2);Floor(5,3);Floor(5,4);Floor(5,5);Floor(5,6);Floor(5,7);
-  Path(6,0);Path(6,1);Floor(6,2);Floor(6,3);Floor(6,4);Floor(6,5);Floor(6,6);Floor(6,7);
-  Floor(7,0);Floor(7,1);Floor(7,2);Floor(7,3);Floor(7,4);Floor(7,5);Floor(7,6);Floor(7,7)
-]
+let rec to_remove enemy_lst path =
+  match enemy_lst with
+  | [] -> 0
+  | Enemy(h,l)::tail ->
+    if List.length path < l
+    then 1 + to_remove tail path
+    else to_remove tail path
+
+let enemy_to_coord enemy path =
+match enemy with
+| Enemy(h,l) -> (List.nth path (l-1))
+
+let enemy_list_pos_to_coord enemy_lst path =
+  List.map (fun (Enemy(h,l)) -> (List.nth path (l-1))) enemy_lst
+
+let distance_t x1 y1 t =
+  match t with
+  |TowerFirst (x2,y2)-> 
+    truncate (floor(sqrt (((float_of_int(x1)-.float_of_int(x2))**2.0)+.((float_of_int(y1)-.float_of_int(y2))**2.0))))
+
+let rec enemies_in_range tower enemy_c_l path range= 
+  match enemy_c_l with
+  | [] -> []
+  | Path(x,y)::tail -> 
+    if distance_t x y tower <= range
+    then (Path(x,y))::enemies_in_range tower tail path range
+    else enemies_in_range tower tail path range
 
 (*entire grid 12X12*)
 let level_1 ={
-    path =        level_1_path_init (level_1_board_init);
+    path =        [Path(0,0);Path(0,1);Path(0,2);Path(0,3);Path(0,4);Path(0,5);Path(0,6);Path(0,7);Path(0,8);Path(0,9)];
     path_start =  (0,0);
     path_end =    (0,0);
     lives =       100;
     enemy_list =  [Enemy(1,1);Enemy(0,1);Enemy(2,2);Enemy(-1,1)];
     tower_first = [];
+    wave = [2;2;2;2;2;2]
     }
 
-let move_enemies enemy_lst =
-  List.map (fun (Enemy(h,l)) -> (Enemy(h,(l+1)))) enemy_lst
+let move_enemies enemy_list =
+  List.map (fun (Enemy(h,l)) -> (Enemy(h,(l+1)))) enemy_list
 
 let rec remove_dead_enemy (l:tile list) =
 match l with
@@ -93,10 +116,23 @@ let loosing_state (lev:level) =
 lev.lives <= 0
 
 let construct_level (lev:level) =
-print_string (String.concat " " (list_to_string_helper level_1.path))
+print_string ("Towers:" ^(string_of_int (List.length (lev.tower_first)))^"\nLives: "
+^string_of_int lev.lives^"\n"^String.concat " " (list_to_string_helper level_1.path));
+print_newline ()
+
+let apply_update_level (lev:level) = {
+    path =        [Path(0,0);Path(1,0);Path(2,0);Path(3,0);Path(4,0);Path(5,0);Path(6,0);Path(7,0);Path(8,0);Path(9,0)];
+    path_start =  (0,0);
+    path_end =    (9,0);
+    lives =       100;
+    enemy_list =  move_enemies lev.enemy_list;
+    tower_first = [];
+    wave = [];
+}
 
 let rec main_loop (lev:level) = 
   if loosing_state lev
     then print_string "YOU LOOSE"
   else 
+    let lev = apply_update_level
     construct_level lev
